cb30a9c48d1cc98a6e8031a621f9d36646ae052b
quochn-RisingStar <103491433+quochn-RisingStar@users.noreply.github.com>
2024-01-24 10:35:14 +0700

    Update main.swift

--- Sources/SplashTokenizer/main.swift
+++ Sources/SplashTokenizer/main.swift
@@ -7,11 +7,6 @@
 import Foundation
 import Splash
 
-guard CommandLine.arguments.count > 1 else {
-    print("⚠️  Please supply the code to tokenize as a string argument")
-    exit(1)
-}
-
-let code = CommandLine.arguments[1]
-let highlighter = SyntaxHighlighter(format: TokenizerOutputFormat())
-print(highlighter.highlight(code))
+let codeTokenizer = CommandLine.arguments[1]
+let highlighterTokenizer = SyntaxHighlighter(format: TokenizerOutputFormat())
+print(highlighter.highlight(codeTokenizer))
